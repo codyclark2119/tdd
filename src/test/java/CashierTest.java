b3332e661d512efb70db1dc5341a2ab3bae9cf6c
Cody Clark <codyclark2119@gmail.com>
2020-07-19 13:12:54 -0500

    Added cashier tests

--- src/test/java/CashierTest.java
+++ src/test/java/CashierTest.java
@@ -9,9 +9,12 @@ import static org.junit.jupiter.api.Assertions.fail;
 class CashierTest {
     Store store = new Store("Wal-Mart");
     Customer customer = new Customer("Jeff");
-    Department dept = new Department("books");
-    List<Item> exItem = Arrays.asList(new Item[]{new Item("I, Robot", false, dept)});
-
+    Department bookDept = new Department("books");
+    List<Item> exItem1 = Arrays.asList(new Item[]{new Item("I, Robot", false, bookDept)});
+    Department eleDept = new Department("electronics");
+    List<Item> exItem2 = Arrays.asList(new Item[]{new Item("Malibu", false, eleDept)});
+    Department foodDept = new Department("food");
+    List<Item> exItem3 = Arrays.asList(new Item[]{new Item("Hersey's Chocolate Bar", false, foodDept)});
 
     @Test
     public void doesExist(){
@@ -26,7 +29,15 @@ class CashierTest {
     @Test
     public void totalTax(){
         try{
-
+            List<Department> storeDeptSeed = Arrays.asList(new Department[]{bookDept, eleDept, foodDept});
+            store.setDepartments(storeDeptSeed);
+            bookDept.setItems(exItem1);
+            eleDept.setItems(exItem2);
+            foodDept.setItems(exItem3);
+            customer.addToCart(store.searchStore("I, Robot"));
+            customer.addToCart(store.searchStore("Malibu"));
+            customer.addToCart(store.searchStore("Hersey's Chocolate Bar"));
+            assertEquals(1.50, store.getCashier().totalTax(customer));
         } catch (RuntimeException e){
             fail();
         }
@@ -35,7 +46,15 @@ class CashierTest {
     @Test
     public void totalPrice(){
         try{
-
+            List<Department> storeDeptSeed = Arrays.asList(new Department[]{bookDept, eleDept, foodDept});
+            store.setDepartments(storeDeptSeed);
+            bookDept.setItems(exItem1);
+            eleDept.setItems(exItem2);
+            foodDept.setItems(exItem3);
+            customer.addToCart(store.searchStore("I, Robot"));
+            customer.addToCart(store.searchStore("Malibu"));
+            customer.addToCart(store.searchStore("Hersey's Chocolate Bar"));
+            assertEquals(1.50, store.getCashier().totalPrice(customer));
         } catch (RuntimeException e){
             fail();
         }
